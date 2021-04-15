7b17142ad37d05635391c0a1cb33ac0e53ee56e8
Quentin Cruz <napu0515@gmail.com>
2021-04-14 17:23:07 -0700

    added new portions to index and sales

--- html/cookie-s.js
+++ html/cookie-s.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Array for hours of operation
-const hoursOfOperationArray = ['6:00am', '7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
+const hoursOfOperationArray = ['6:00am ', '7:00am ','8:00am ','9:00am ','10:00am ' ,'11:00am ','12:00pm ','1:00pm ','2:00pm ','3:00pm ','4:00pm ','5:00pm ','6:00pm ','7:00pm '];
 
 // Constructor function for SalmonCookieStores
 function SalmonCookieStores(name,minCustomersPerHour,maxCustomersPerHour,averageCookieSale,) {
